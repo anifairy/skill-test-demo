532f7942c29bfddeca9a365e4d90ef07e2c8b532
anifairy <43860686+anifairy@users.noreply.github.com>
2022-05-04 23:11:14 -0700

    added file

---
+++ SkillDemo2Test.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+
+public class SkillDemo2Test{
+
+    @Test
+    public void test1() {
+    assertEquals(2,SkillDemo2.multiply(2,3); // failing test
+    }
+
+}
